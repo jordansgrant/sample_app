4ed74aa42c8cf854f4c8de33ba3c6b96ca7bfd87
jordansgrant <jordans.grant@gmail.com>
2017-07-03 18:30:02 -0400

    Added migration for password_digest, added bcrypt 3.1.11 gem for password hash, added 'has_secure_passwords' to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(fname: "Example", lname: "User", email: "example.user@domain.com")
+    @user = User.new(fname: "Example", lname: "User", email: "example.user@domain.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid user" do
