93b4fda798c08c91785282fb16c814813c3dd291
jordansgrant <jordans.grant@gmail.com>
2017-07-04 18:46:28 -0400

    Implement basic login finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to user_url(@user)
     else
@@ -21,18 +22,6 @@ class UsersController < ApplicationController
     end
   end
   
-  
-  def edit
-    
-  end
-  
-  def update
-    
-  end
-  
-  def destroy
-    
-  end
   
   private
     def user_params
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,5 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_select "div.alert-success"
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
